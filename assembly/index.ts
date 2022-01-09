35179b1c85afebd3790f094379b0b3d530870a56
AsterixxxGallier <asterixxxgallier@aol.com>
2022-01-09 22:32:17 +0100

    feat: add LinkIndex class

--- assembly/index.ts
+++ assembly/index.ts
@@ -28,6 +28,23 @@ class ChunkedFlexList<E, D extends number> {
 	}
 }
 
+/**
+ * Stores the "index" of a link, determined by the index of the node the link starts from and the degree of that link.
+ */
+class LinkIndex {
+	nodeIndex: u8
+	degree: u8
+
+	constructor(nodeIndex: u8, degree: u8) {
+		this.nodeIndex = nodeIndex
+		this.degree = degree
+	}
+
+	toString(): string {
+		return `[node ${this.nodeIndex.toString(2).padStart(Chunk.indexBits, '0')} degree ${this.degree}]`
+	}
+}
+
 abstract class Chunk<E, D extends number> {
 
 }
