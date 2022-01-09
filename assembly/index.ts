85696055bb2e0542af8eeb769c9807f35d2b6f4d
AsterixxxGallier <asterixxxgallier@aol.com>
2022-01-09 12:33:07 +0100

    feat: add ChunkedFlexList stub

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,29 @@
-// The entry file of your WebAssembly module.
+/**
+ * A list-like data structure that stores the distance between its elements.
+ * - Separates data into nested chunks of 256 elements/chunks each.
+ * - Each chunk is allocated at once, so for 256 elements only one allocation is needed.
+ * - Has a structure akin to binary trees which allows for very fast traversal (O(log₂ n)).
+ * - Doesn't store absolute position of elements so moving them (changing their position, not their index) is very fast and O(1).
+ * - Very fast and O(1) addition of new elements, although every 256th addition will necessitate the allocation of a new chunk.
+ * - Splicing elements into the list does not move the other elements in any way and is O(log₂ n), but makes the structure slightly less efficient
+ *   because it inserts a sublist between two indices.
+ * - Every element will stay at the same index for the lifetime of the list.
+ * - Because of sublists, references to elements should be stored as a chain of indices.
+ * - Stores reference to last chunk for speed.
+ * - Stores positional offset for the whole list that offsets all elements.
+ * - E is the type of data to be stored, D is the number type to be used for storing distances between elements and the list offset.
+ */
+class ChunkedFlexList<E, D extends number> {
+	/**
+	 * Offsets all elements (distance between zero and the first element)
+	 */
+	offset: D
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+	/**
+	 * Creates an empty ChunkedFlexList
+	 * @param offset see {@link ChunkedFlexList.offset}
+	 */
+	constructor(offset: D = 0 as D) {
+		this.offset = offset
+	}
 }
