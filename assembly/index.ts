518a5ad6521644857bcaea999a1bd445cd84b0d2
AsterixxxGallier <asterixxxgallier@aol.com>
2022-01-09 22:32:54 +0100

    feat: add static fields and their respective initialization methods to Chunk

--- assembly/index.ts
+++ assembly/index.ts
@@ -46,7 +46,42 @@ class LinkIndex {
 }
 
 abstract class Chunk<E, D extends number> {
+	static indexBits: i32 = 8
+	static maxSize: i32 = 1 << Chunk.indexBits
+	static numbersOfLinks: StaticArray<u8> = new StaticArray<u8>(Chunk.maxSize)
+	static linkIndexesAbove: StaticArray<Array<LinkIndex>> =
+		new StaticArray<Array<LinkIndex>>(Chunk.maxSize)
 
+	@inline
+	static calculateLinkIndexesAbove(nodeIndex: u8): Array<LinkIndex> {
+		if (nodeIndex == 0) return new Array<LinkIndex>(0)
+		const linkIndexes = new Array<LinkIndex>(this.indexBits as i32)
+		let index = nodeIndex
+		for (let degree = 0 as u8; degree < (this.indexBits as u8); degree++) {
+			if ((nodeIndex as i32) + (1 << degree) > this.maxSize) {
+				linkIndexes.length = degree
+				return linkIndexes
+			}
+			if (degree == 0) index--
+			linkIndexes[degree] = new LinkIndex(index, degree)
+			index = index & ~(1 << degree)
+		}
+		return linkIndexes
+	}
+
+	@inline
+	static calculateNumberOfLinks(index: u8): u8 {
+		const trailingZeros = ctz(index)
+		const ones = popcnt(index)
+		return Chunk.indexBits - trailingZeros > (ones as i32) ? trailingZeros + 1 : trailingZeros
+	}
+
+	static init(): void {
+		for (let i = 0; i < Chunk.maxSize; i++) {
+			this.numbersOfLinks[i] = this.calculateNumberOfLinks(i as u8)
+			this.linkIndexesAbove[i] = this.calculateLinkIndexesAbove(i as u8)
+		}
+	}
 }
 
 /**
