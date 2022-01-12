91642e1290e400d99c7f074796ded0973d2498fa
AsterixxxGallier <asterixxxgallier@aol.com>
2022-01-12 16:50:33 +0100

    docs: document HollowChunk and HollowChunkedFlexList

--- assembly/hollowChunk.ts
+++ assembly/hollowChunk.ts
@@ -1,5 +1,8 @@
 import {AbstractChunk} from "./abstractChunk"
 
+/**
+ * A single chunk that stores the distance between dataless points instead of list elements.
+ */
 export class HollowChunk<D extends number> extends AbstractChunk<D> {
 
 }
--- assembly/hollowChunkedFlexList.ts
+++ assembly/hollowChunkedFlexList.ts
@@ -1,6 +1,9 @@
 import {AbstractChunkedFlexList} from "./abstractChunkedFlexList"
 import {HollowChunk} from "./hollowChunk"
 
+/**
+ * A data structure that stores the distances between dataless points instead of list elements.
+ */
 export class HollowChunkedFlexList<D extends number> extends AbstractChunkedFlexList<D, HollowChunk<D>> {
 
 }
