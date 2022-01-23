e7e14497e169f272b68dd95324c58839ef7d5a52
AsterixxxGallier <asterixxxgallier@aol.com>
2022-01-23 16:22:56 +0100

    refactor: move appendElementUnchecked to DatafulChunk

--- assembly/datafulChunk.ts
+++ assembly/datafulChunk.ts
@@ -7,4 +7,15 @@ export abstract class DatafulChunk<E, D extends number> extends AbstractChunk<D>
 	abstract getElementAt(index: u8): E
 
 	abstract setElementAt(index: u8, element: E): void
+
+	/**
+	 * Appends an element to this chunk, distanceFromEnd away from the last element.
+	 * Internally calls {@link appendNodeUnchecked} after storing the given element using {@link setElementAt}.
+	 * @param distanceFromEnd
+	 * @param element
+	 */
+	appendElementUnchecked(element: E, distanceFromEnd: D) {
+		this.setElementAt(this.size, element)
+		this.appendNodeUnchecked(distanceFromEnd)
+	}
 }
