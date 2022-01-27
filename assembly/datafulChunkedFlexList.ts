30d338bb8749332836ab6bf362acf4f149b5c31f
AsterixxxGallier <asterixxxgallier@aol.com>
2022-01-27 21:44:07 +0100

    feat: add DatafulChunkedFlexList.appendElement method

--- assembly/datafulChunkedFlexList.ts
+++ assembly/datafulChunkedFlexList.ts
@@ -1,7 +1,15 @@
 import {AbstractChunkedFlexList} from "./abstractChunkedFlexList"
+import {DatafulChunk} from "./datafulChunk";
 
 /**
  * Superclass for ChunkedFlexList and RangeChunkedFlexList
  */
 export abstract class DatafulChunkedFlexList<E, D extends number> extends AbstractChunkedFlexList<D> {
+	appendElement(element: E, distanceFromEnd: D): void {
+		if (this.size == 0)
+			this.offset = this.offset + distanceFromEnd as D
+		this.makeSpace();
+		(this.lastChunks[0] as DatafulChunk<E, D>).appendElementUnchecked(element, distanceFromEnd)
+		this.size++
+	}
 }
